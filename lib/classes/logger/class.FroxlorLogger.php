2acd69f2e79825ad774749a384843065781275ff
Florian Aders (EleRas) <eleras@froxlor.org>
2010-01-22 15:10:26 +0000

    Renamed SysCPLogger -> FroxlorLogger

--- lib/classes/logger/class.FroxlorLogger.php
+++ lib/classes/logger/class.FroxlorLogger.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Logger - SysCP-Logger Class
+ * Logger - Froxlor-Logger Class
  *
  *
  * This program is free software; you can redistribute it and/or modify
@@ -16,11 +16,11 @@
  * @author     Michael Kaufmann <mkaufmann@nutime.de>
  * @license    http://www.gnu.org/licenses/gpl.txt
  * @package    Functions
- * @version    CVS: $Id: class.SysCPLogger.php 2724 2009-06-07 14:18:02Z flo $
+ * @version    CVS: $Id: class.froxlorLogger.php 2724 2009-06-07 14:18:02Z flo $
  * @link       http://www.nutime.de/
  */
 
-class SysCPLogger
+class FroxlorLogger
 {
 	/**
 	 * Userinfo
@@ -109,7 +109,7 @@ class SysCPLogger
 
 		if(!isset(self::$loggers[$_usernfo['loginname']]))
 		{
-			self::$loggers[$_usernfo['loginname']] = new SysCPLogger($_usernfo, $_db, $_settings);
+			self::$loggers[$_usernfo['loginname']] = new FroxlorLogger($_usernfo, $_db, $_settings);
 		}
 
 		return self::$loggers[$_usernfo['loginname']];
--- lib/init.php
+++ lib/init.php
@@ -2,20 +2,22 @@
 
 /**
  * This file is part of the Froxlor project.
- * Copyright (c) 2003-2009 the Froxlor Team (see authors).
+ * Copyright (c) 2003-2009 the SysCP Team (see authors).
+ * Copyright (c) 2010 the Froxlor Team (see authors).
  *
  * For the full copyright and license information, please view the COPYING
  * file that was distributed with this source code. You can also view the
- * COPYING file online at http://files.syscp.org/misc/COPYING.txt
+ * COPYING file online at http://files.froxlor.org/misc/COPYING.txt
  *
  * @copyright  (c) the authors
- * @author     Florian Lippert <flo@syscp.org>
- * @license    GPLv2 http://files.syscp.org/misc/COPYING.txt
+ * @author     Florian Lippert <flo@syscp.org> (2003-2009)
+ * @author     Froxlor Teami <team@froxlor.org.org> (2010)
+ * @license    GPLv2 http://files.froxlor.org/misc/COPYING.txt
  * @package    Panel
  * @version    $Id: init.php 2733 2009-11-06 09:32:00Z flo $
  */
 
-// prevent syscp pages from being cached
+// prevent Froxlor pages from being cached
 
 header("Cache-Control: no-cache, must-revalidate");
 header("Pragma: no-cache");
