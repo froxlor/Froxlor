24a5e05bf90b6838fbb41f0dc9442063c9c74fbb
Michael Kaufmann (d00p) <d00p@froxlor.org>
2010-01-31 13:17:58 +0000

    removing uneeded header.png and unecessary sql-update

--- install/updates/froxlor/0.9/update_0.9.inc.php
+++ install/updates/froxlor/0.9/update_0.9.inc.php
@@ -324,13 +324,6 @@ if(isFroxlorVersion('0.9-r3'))
 if(isFroxlorVersion('0.9-r4'))
 {
 	showUpdateStep("Updating from 0.9-r4 to 0.9 final", false);
-	
-	if($settings['admin']['froxlor_graphic'] == 'images/header.gif')
-	{
-		showUpdateStep("Changing header graphic");
-		$db->query("UPDATE `" . TABLE_PANEL_SETTINGS . "` SET `value` = 'images/header.png' WHERE `settinggroup`='admin' AND `varname`='froxlor_graphic'");		
-		lastStepStatus(0);
-	}
 	updateToVersion('0.9');
 }
 
