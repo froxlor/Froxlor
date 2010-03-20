dcb32c1bb5fe9d945015278af89d8fafe5b6db95
Michael Kaufmann (d00p) <d00p@froxlor.org>
2010-03-20 14:44:47 +0000

    - use makeCorrectDir() on fcgid-tmp-directory (trailing slash needed)

--- admin_configfiles.php
+++ admin_configfiles.php
@@ -98,7 +98,7 @@ if($userinfo['change_serversettings'] == '1')
 			'<AWSTATS_PATH>' => $settings['system']['awstats_path'],
 			'<SSLPROTOCOLS>' => ($settings['system']['use_ssl'] == '1') ? 'imaps pop3s' : '',
 			'<REALTIME_PORT>' => $settings['system']['realtime_port'],
-			'<CUSTOMER_TMP>' => ($settings['system']['mod_fcgid_tmpdir'] != '') ? $settings['system']['mod_fcgid_tmpdir'] : '/tmp/'
+			'<CUSTOMER_TMP>' => ($settings['system']['mod_fcgid_tmpdir'] != '') ? makeCorrectDir($settings['system']['mod_fcgid_tmpdir']) : '/tmp/'
 		);
 		$files = '';
 		$configpage = '';
