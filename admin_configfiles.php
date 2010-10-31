03a8a9c8c98ad966a00831e66ac81b1582fbe515
Michael Kaufmann (d00p) <d00p@froxlor.org>
2010-10-31 08:29:39 +0000

    - remove some more realtime-leftovers

--- admin_configfiles.php
+++ admin_configfiles.php
@@ -96,7 +96,6 @@ if($userinfo['change_serversettings'] == '1')
 			'<VIRTUAL_UID_MAPS>' => $settings['system']['vmail_uid'],
 			'<VIRTUAL_GID_MAPS>' => $settings['system']['vmail_gid'],
 			'<SSLPROTOCOLS>' => ($settings['system']['use_ssl'] == '1') ? 'imaps pop3s' : '',
-			'<REALTIME_PORT>' => $settings['system']['realtime_port'],
 			'<CUSTOMER_TMP>' => ($settings['system']['mod_fcgid_tmpdir'] != '') ? makeCorrectDir($settings['system']['mod_fcgid_tmpdir']) : '/tmp/'
 		);
 		$files = '';
