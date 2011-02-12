9e798c636201320b484070e6028d989eea8dbd3a
Michael Kaufmann (d00p) <d00p@froxlor.org>
2011-02-12 16:13:57 +0100

    add formfields for customer_extras

---
+++ lib/formfields/customer/extras/formfield.htpasswd_edit.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * This file is part of the Froxlor project.
+ * Copyright (c) 2010 the Froxlor Team (see authors).
+ *
+ * For the full copyright and license information, please view the COPYING
+ * file that was distributed with this source code. You can also view the
+ * COPYING file online at http://files.froxlor.org/misc/COPYING.txt
+ *
+ * @copyright  (c) the authors
+ * @author     Froxlor team <team@froxlor.org> (2010-)
+ * @license    GPLv2 http://files.froxlor.org/misc/COPYING.txt
+ * @package    Formfields
+ * @version    $Id: formfield.domains_add.php 112 2010-12-14 12:11:20Z d00p $
+ */
+
+return array(
+	'htpasswd_edit' => array(
+		'title' => $lng['extras']['directoryprotection_add'],
+		'image' => 'icons/add_htpasswd.png',
+		'sections' => array(
+			'section_a' => array(
+				'title' => $lng['extras']['directoryprotection_add'],
+				'image' => 'icons/add_htpasswd.png',
+				'fields' => array(
+					'path' => array(
+						'label' => $lng['panel']['path'],
+						'type' => 'label',
+						'value' => $result['path']
+					),
+					'username' => array(
+						'label' => $lng['login']['username'],
+						'type' => 'label',
+						'value' => $result['username']
+					),
+					'directory_password' => array(
+						'label' => $lng['login']['password'],
+						'type' => 'password'
+					),
+					'directory_authname' => array(
+						'label' => $lng['extras']['htpasswdauthname'],
+						'type' => 'text',
+						'value' => $result['authname']
+					)
+				)
+			)
+		)
+	)
+);
