465f5b406ee1fe879b78e2997e8772cc5eb768e5
Michael Kaufmann (d00p) <d00p@froxlor.org>
2011-03-10 12:53:08 +0100

    - fix checkbox-styling when using opera - fix style-issue with table-hover
    Signed-off-by: Michael Kaufmann (d00p) <d00p@froxlor.org>

--- templates/Froxlor/js/froxlor.js
+++ templates/Froxlor/js/froxlor.js
@@ -1,41 +1,39 @@
-$(document).ready(function(){
+$(document).ready(function() {
 	// make rel="external" links open in a new window
-	$("a[rel='external']").attr('target', '_blank');
-	$(".main").css('min-height', $("nav").height() - 34);
-	$(".dboarditem:last").css('min-height', $(".dboarditem:first").height());
+		$("a[rel='external']").attr('target', '_blank');
+		$(".main").css('min-height', $("nav").height() - 34);
+		$(".dboarditem:last")
+				.css('min-height', $(".dboarditem:first").height());
 
-	// set focus on username-field if on loginpage
-	if($(".loginpage").length != 0) {
-		$("#loginname").focus();
-	}
-
-	if($("table.formtable").length != 0) {
-		$("table.formtable tr").hover(function() {
-			$(this).css("background-color", "#fff");
-		},
-		function() {
-			$(this).css("background-color", "#f5f5f5");
+		// set focus on username-field if on loginpage
+		if ($(".loginpage").length != 0) {
+			$("#loginname").focus();
 		}
-		);
-	}
-	if($("table.bradiusodd").length != 0) {
-		$("table.bradiusodd tr").hover(function() {
-			$(this).css("background-color", "#fff");
-		},
-		function() {
-			$(this).css("background-color", "#f5f5f5");
+
+		if ($("table.formtable").length != 0) {
+			$("table.formtable tr").hover(function() {
+				$(this).css("background-color", "#fff");
+			}, function() {
+				$(this).css("background-color", "#f5f5f5");
+			});
 		}
-		);
-	}
+		if ($("table.bradiusodd").leingth != 0) {
+			$("table.bradiusodd tbody tr").not(':last-child').hover(function() {
+				$(this).css("background-color", "#fff");
+			}, function() {
+				$(this).css("background-color", "#f5f5f5");
+			});
+			// last row needs border-radius
+			$("table.bradiusodd tbody tr:last-child").hover(function() {
+				 $(this).children().css("background-color", "#fff");
+				 $(this).children(':first-child').css("-webkit-border-bottom-left-radius", "20px");
+				 $(this).children(':first-child').css("-moz-border-radius-bottomleft", "20px");
+				 $(this).children(':first-child').css("border-bottom-left-radius", "20px");
+			},
+			function() {
+				$(this).children().css("background-color", "#f5f5f5");
+			}
+			);
+		}
 
-        if($("table.aps").length != 0) {
-                $("table.aps tr").hover(function() {
-                        $(this).css("background-color", "#fff");
-                },
-                function() {
-                        $(this).css("background-color", "#f5f5f5");
-                }
-                );
-        }
-
-});
+	});
