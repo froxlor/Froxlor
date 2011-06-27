8f760e87bc198672b75471faec82693e7561e58e
Florian Aders (EleRas) <eleras@froxlor.org>
2011-06-27 18:13:47 +0200

    Tagging 0.9.22-rc1
    Signed-off-by: Florian Aders (EleRas) <eleras@froxlor.org>

--- install/updates/froxlor/0.9/update_0.9.inc.php
+++ install/updates/froxlor/0.9/update_0.9.inc.php
@@ -1645,3 +1645,11 @@ if(isFroxlorVersion('0.9.22-svn1'))
 	
 	updateToVersion('0.9.22-svn2');
 }
+
+if(isFroxlorVersion('0.9.22-svn2'))
+{
+	showUpdateStep("Updating from 0.9.22-svn2 to 0.9.22-rc1");
+	lastStepStatus(0);
+	
+	updateToVersion('0.9.22-rc1');
+}
