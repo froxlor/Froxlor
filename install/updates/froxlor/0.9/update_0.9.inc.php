65a5df507c31fcdcf4f8bbc9208adfcabf769373
Florian Aders (EleRas) <eleras@froxlor.org>
2011-10-31 12:20:17 +0100

    Tagging 0.9.25-rc1
    Signed-off-by: Florian Aders (EleRas) <eleras@froxlor.org>

--- install/updates/froxlor/0.9/update_0.9.inc.php
+++ install/updates/froxlor/0.9/update_0.9.inc.php
@@ -1709,3 +1709,12 @@ if(isFroxlorVersion('0.9.24-rc1'))
 	updateToVersion('0.9.24');
 }
 
+if(isFroxlorVersion('0.9.24'))
+{
+	showUpdateStep("Updating from 0.9.24 to 0.9.25-rc1");
+	lastStepStatus(0);
+
+	updateToVersion('0.9.25-rc1');
+}
+
+
