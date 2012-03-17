604b1dc912e6b5fff8f463f3a0b2c42d8db368e4
Charles Williams (Slydder) <chuck@itadmins.net>
2012-03-17 14:27:03 +0100

    fixed php-fpm config regeneration when  is missing values

--- scripts/jobs/cron_tasks.inc.http.10.apache.php
+++ scripts/jobs/cron_tasks.inc.http.10.apache.php
@@ -1298,6 +1298,11 @@ class apache
 		}
 		if($this->settings['phpfpm']['enabled'] == '1')
 		{
+			foreach($this->virtualhosts_data as $vhosts_filename => $vhosts_file)
+			{
+				$this->known_vhostfilenames[] = basename($vhosts_filename);
+			}
+
 			foreach($this->known_vhostfilenames as $vhostfilename){
 				$known_phpfpm_files[]=preg_replace('/^(05|10|20|21|22|30|50|51)_(froxlor|syscp)_(dirfix|ipandport|normal_vhost|wildcard_vhost|ssl_vhost)_/', '', $vhostfilename);
 			}
@@ -1349,6 +1354,11 @@ class apache
 		}
 		if($this->settings['phpfpm']['enabled'] == '1')
 		{
+			foreach($this->virtualhosts_data as $vhosts_filename => $vhosts_file)
+			{
+				$this->known_vhostfilenames[] = basename($vhosts_filename);
+			}
+
 			foreach($this->known_vhostfilenames as $vhostfilename){
 				$known_phpfpm_files[]=preg_replace('/^(05|10|20|21|22|30|50|51)_(froxlor|syscp)_(dirfix|ipandport|normal_vhost|wildcard_vhost|ssl_vhost)_/', '', $vhostfilename);
 			}
@@ -1358,7 +1368,8 @@ class apache
 		
 			while(false !== ($phpfpm_filename = readdir($phpfpm_file_dirhandle)))
 			{
-				if($phpfpm_filename != '.'
+				if(is_array($known_phpfpm_files)
+					&& $phpfpm_filename != '.'
 					&& $phpfpm_filename != '..'
 					&& !in_array($phpfpm_filename, $known_phpfpm_files)
 					&& file_exists(makeCorrectFile($this->settings['phpfpm']['configdir'] . '/' . $phpfpm_filename)))
@@ -1366,6 +1377,10 @@ class apache
 						fwrite($this->debugHandler, '  apache::wipeOutOldVhostConfigs: unlinking PHP5-FPM ' . $phpfpm_filename . "\n");
 						$this->logger->logAction(CRON_ACTION, LOG_NOTICE, 'unlinking ' . $phpfpm_filename);
 						unlink(makeCorrectFile($this->settings['phpfpm']['configdir'] . '/' . $phpfpm_filename));
+					}
+				if(!is_array($known_phpfpm_files))
+					{
+						$this->logger->logAction(CRON_ACTION, LOG_WARNING, "WARNING!! PHP-FPM Configs Not written!!");
 					}
 			}
 		}
