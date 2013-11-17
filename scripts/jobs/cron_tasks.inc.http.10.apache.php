ed1c87c521cee64bc4dbe72a8451943cc7dd000a
Michael Kaufmann (d00p) <d00p@froxlor.org>
2013-11-17 19:29:35 +0100

    as fcgid does not add a handler in a separate config such as fpm does, in the ip/port vhost-config of course, this has to be SetHandler instead of AddHandler, thx to quizzi
    Signed-off-by: Michael Kaufmann (d00p) <d00p@froxlor.org>

--- scripts/jobs/cron_tasks.inc.http.10.apache.php
+++ scripts/jobs/cron_tasks.inc.http.10.apache.php
@@ -242,7 +242,7 @@ class apache
 						$this->virtualhosts_data[$vhosts_filename].= '  <Directory "' . $mypath . '">' . "\n";
 						$file_extensions = explode(' ', $phpconfig['file_extensions']);
 						$this->virtualhosts_data[$vhosts_filename].= '    <FilesMatch "\.(' . implode('|', $file_extensions) . ')$">' . "\n";
-						$this->virtualhosts_data[$vhosts_filename].= '      AddHandler fcgid-script .php' . "\n";
+						$this->virtualhosts_data[$vhosts_filename].= '      SetHandler fcgid-script' . "\n";
 						foreach ($file_extensions as $file_extension) {
 							$this->virtualhosts_data[$vhosts_filename].= '      FcgidWrapper ' . $starter_filename . ' .' . $file_extension . "\n";
 						}
