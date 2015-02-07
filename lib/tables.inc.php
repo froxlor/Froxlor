f1360d879283a728f82aa8d5c79ecdd346e25e81
Michael Kaufmann (d00p) <d00p@froxlor.org>
2015-02-07 09:55:52 +0100

    set version to 0.9.33 for upcoming stable release
    Signed-off-by: Michael Kaufmann (d00p) <d00p@froxlor.org>

--- lib/tables.inc.php
+++ lib/tables.inc.php
@@ -51,6 +51,6 @@ define('TABLE_PANEL_DOMAIN_SSL_SETTINGS', 'domain_ssl_settings');
 define('TABLE_DOMAINTOIP', 'panel_domaintoip');
 
 // VERSION INFO
-$version = '0.9.33-rc3';
+$version = '0.9.33';
 $dbversion = '2';
 $branding = '';
