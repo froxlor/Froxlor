c9118d1bf85f636b283df66aeaf7b5a60fc118c3
Chris Vigelius <me@cv.gd>
2015-04-28 20:45:42 +0200

    use settings class instead of handcrafted sql

--- install/updates/froxlor/0.9/update_0.9.inc.php
+++ install/updates/froxlor/0.9/update_0.9.inc.php
@@ -2982,6 +2982,6 @@ if (isFroxlorVersion('0.9.34-dev3')) {
     showUpdateStep("Updating from 0.9.34-dev3 to 0.9.34-dev4", false);
 
     showUpdateStep("Adding new setting for customer dir group");
-    Database::query("INSERT INTO `panel_settings` SET `settinggroup` = 'system', `varname` = 'customerdir_group_webserver', `value` = '0';");
+    Settings::AddNew('system.customerdir_group_webserver', '0');
     updateToVersion('0.9.34-dev4');
 }
