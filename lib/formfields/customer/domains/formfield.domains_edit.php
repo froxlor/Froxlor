34092b248ea2fc2b33ad1a01c419e32617f78872
Dave <mdxdave@gmail.com>
2015-12-10 05:31:48 +0100

    Update formfield.domains_edit.php

--- lib/formfields/customer/domains/formfield.domains_edit.php
+++ lib/formfields/customer/domains/formfield.domains_edit.php
@@ -93,14 +93,14 @@ return array(
 						'select_var' => $openbasedir
 					),
 					'hhvm' => array(
-						'visible' => (Settings::Get('system.can_use_hhvm') == '1'),
+						'visible' => (Settings::Get('system.mod_fcgid_hhvm') == '1' && Settings::Get('system.mod_fcgid') == '1'),
 						'label' => $lng['domains']['hhvm']['title'],
 						'desc' => $lng['domains']['hhvm']['description'],
 						'type' => 'checkbox',
 						'values' => array(
 										array ('label' => $lng['panel']['yes'], 'value' => '1')
 									),
-						'value' => array($result['hhvm'])
+						'value' => array($result["hhvm"])
 					)
 				)
 			)
