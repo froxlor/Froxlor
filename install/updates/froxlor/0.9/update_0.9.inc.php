25656b2cc76f61bc0d5aebf350f03579599fd3d9
Dave <mdxdave@gmail.com>
2015-12-10 06:16:13 +0100

    Fix

--- install/updates/froxlor/0.9/update_0.9.inc.php
+++ install/updates/froxlor/0.9/update_0.9.inc.php
@@ -3002,7 +3002,7 @@ if (isFroxlorVersion('0.9.34-dev4')) {
     showUpdateStep("Updating from 0.9.34-dev4 to 0.9.34-dev5", false);
 
     showUpdateStep("Adding field hhvm to domains table");
-    Database::query("ALTER TABLE `".TABLE_PANEL_DOMAINS."` ADD `hhvm` int(1) NOT NULL DEFAULT '0`");
+    Database::query("ALTER TABLE `".TABLE_PANEL_DOMAINS."` ADD `hhvm` int(1) NOT NULL DEFAULT 0");
     lastStepStatus(0);
 
     updateToVersion('0.9.34-dev5');
