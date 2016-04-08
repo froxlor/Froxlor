533112720463fdb712140d116e058d9c223cc00e
Michael Kaufmann (d00p) <d00p@froxlor.org>
2016-04-08 13:52:06 +0200

    set version to 0.9.35.1; fix updater :x
    Signed-off-by: Michael Kaufmann (d00p) <d00p@froxlor.org>

--- install/updates/froxlor/0.9/update_0.9.inc.php
+++ install/updates/froxlor/0.9/update_0.9.inc.php
@@ -3284,3 +3284,11 @@ if (isFroxlorVersion('0.9.35-rc1')) {
 
 	updateToVersion('0.9.35');
 }
+
+if (isFroxlorVersion('0.9.35')) {
+
+	showUpdateStep("Updating from 0.9.35 to 0.9.35.1");
+	lastStepStatus(0);
+
+	updateToVersion('0.9.35.1');
+}
