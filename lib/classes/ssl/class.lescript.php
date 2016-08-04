b2f815617cfd880c83e9cf233cf9ed4f5b68e08a
Michael Kaufmann (d00p) <d00p@froxlor.org>
2016-08-04 07:39:32 +0200

    Update Let's Encrypt subscriber agreement, fixes #1644
    Signed-off-by: Michael Kaufmann (d00p) <d00p@froxlor.org>

--- lib/classes/ssl/class.lescript.php
+++ lib/classes/ssl/class.lescript.php
@@ -29,7 +29,8 @@
 class lescript
 {
 
-	public $license = 'https://letsencrypt.org/documents/LE-SA-v1.0.1-July-27-2015.pdf';
+	//public $license = 'https://letsencrypt.org/documents/LE-SA-v1.0.1-July-27-2015.pdf';
+	public $license = 'https://letsencrypt.org/documents/LE-SA-v1.1.1-August-1-2016.pdf';
 
 	private $logger;
 
