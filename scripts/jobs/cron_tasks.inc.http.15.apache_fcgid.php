0566df14e00317adef1db6b5c540d368cad2e7f3
Philipp Dreimann <philipp@dreimann.net>
2016-11-18 14:53:31 +0100

    typo fix: Settings.Get -> Settings::Get

--- scripts/jobs/cron_tasks.inc.http.15.apache_fcgid.php
+++ scripts/jobs/cron_tasks.inc.http.15.apache_fcgid.php
@@ -37,7 +37,7 @@ class apache_fcgid extends apache
 				// #1317 - perl is executed via apache and therefore, when using fpm, does not know the user
 				// which perl is supposed to run as, hence the need for Suexec need
 				if (customerHasPerlEnabled($domain['customerid'])) {
-					$php_options_text.= '  SuexecUserGroup "' . Settings.Get('system.mod_fcgid_system_prefix') . $domain['loginname'] . '" "' . Settings.Get('system.mod_fcgid_system_prefix') . $domain['loginname'] . '"' . "\n";
+					$php_options_text.= '  SuexecUserGroup "' . Settings::Get('system.mod_fcgid_system_prefix') . $domain['loginname'] . '" "' . Settings::Get('system.mod_fcgid_system_prefix') . $domain['loginname'] . '"' . "\n";
 				}
 				
 				// mod_proxy stuff for apache-2.4
@@ -87,7 +87,7 @@ class apache_fcgid extends apache
 			else
 			{
 				$php_options_text.= '  FcgidIdleTimeout ' . Settings::Get('system.mod_fcgid_idle_timeout') . "\n";
-				$php_options_text.= '  SuexecUserGroup "' . Settings.Get('system.mod_fcgid_system_prefix') . $domain['loginname'] . '" "' . Settings.Get('system.mod_fcgid_system_prefix') . $domain['loginname'] . '"' . "\n";
+				$php_options_text.= '  SuexecUserGroup "' . Settings::Get('system.mod_fcgid_system_prefix') . $domain['loginname'] . '" "' . Settings::Get('system.mod_fcgid_system_prefix') . $domain['loginname'] . '"' . "\n";
 
 				if((int)Settings::Get('system.mod_fcgid_wrapper') == 0)
 				{
