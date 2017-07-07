603b959805961bcd93c4f5e564a785174c149371
Pascal Querner <hello@paskl.de>
2017-07-07 22:35:21 +0200

    #459 set froxlor incl. version as useragent for curl call (like it used to be)

--- lib/classes/ssl/class.lescript.php
+++ lib/classes/ssl/class.lescript.php
@@ -509,7 +509,7 @@ keyUsage = nonRepudiation, digitalSignature, keyEncipherment');
         ];
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322)');
+        curl_setopt($ch, CURLOPT_USERAGENT, 'Froxlor/' . $this->version);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         $data = curl_exec($ch);
         $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
