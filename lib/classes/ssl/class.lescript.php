5540b02e352fc421763d01d4e69beca6fd6ea11d
Michael Kaufmann (d00p) <d00p@froxlor.org>
2017-11-27 07:54:41 +0100

    do not remove Let's Encrypt token when self-check fails but rather give out the information as warning. The self-check fails for many users due to different local configurations and might not always be correct; fixes #480
    Signed-off-by: Michael Kaufmann (d00p) <d00p@froxlor.org>

--- lib/classes/ssl/class.lescript.php
+++ lib/classes/ssl/class.lescript.php
@@ -236,9 +236,7 @@ class lescript
 				} else {
 					$errmsg = "";
 				}
-				@unlink($tokenPath);
-				$this->logger->logAction(CRON_ACTION, LOG_ERR, "letsencrypt Please check $uri - token not available" . $errmsg);
-				continue;
+				$this->logger->logAction(CRON_ACTION, LOG_WARNING, "[Lets Encrypt self-check] Please check $uri - token seems to be not available. This is just a simple self-check, it might be wrong but consider using this information when Let's Encrypt fails to issue a certificate" . $errmsg);
 			}
 
 			$this->log("Sending request to challenge");
