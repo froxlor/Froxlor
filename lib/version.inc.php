281b131c628b07464f28d28e6194f9b04780d521
Michael Kaufmann (d00p) <d00p@froxlor.org>
2018-01-10 10:21:02 +0100

    ups, forgot to adjust version in lib/version.inc.php
    Signed-off-by: Michael Kaufmann (d00p) <d00p@froxlor.org>

--- lib/version.inc.php
+++ lib/version.inc.php
@@ -19,7 +19,7 @@
 $version = '0.9.38.8';
 
 // Database version (YYYYMMDDC where C is a daily counter)
-$dbversion = '201801091';
+$dbversion = '201801100';
 
 // Distribution branding-tag (used for Debian etc.)
 $branding = '';
