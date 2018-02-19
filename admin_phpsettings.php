8c6ae4f3a369ee64b6ad2b6552283c67388b4573
Michael Kaufmann (d00p) <d00p@froxlor.org>
2018-02-19 13:36:15 +0100

    add PhpSettings ApiCommand
    Signed-off-by: Michael Kaufmann (d00p) <d00p@froxlor.org>

--- admin_phpsettings.php
+++ admin_phpsettings.php
@@ -29,73 +29,29 @@ if ($page == 'overview') {
 	
 	if ($action == '') {
 		
+		try {
+			$json_result = PhpSettings::getLocal($userinfo)->list();
+		} catch (Exception $e) {
+			dynamic_error($e->getMessage());
+		}
+		$result = json_decode($json_result, true)['data'];
+
 		$tablecontent = '';
 		$count = 0;
-		$result = Database::query("
-			SELECT c.*, fd.description as fpmdesc
-			FROM `" . TABLE_PANEL_PHPCONFIGS . "` c
-			LEFT JOIN `" . TABLE_PANEL_FPMDAEMONS . "` fd ON fd.id = c.fpmsettingid
-			ORDER BY c.description ASC
-		");
-		
-		while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-			
-			$domainresult = false;
-			$query_params = array(
-				'id' => $row['id']
-			);
-			
-			$query = "SELECT * FROM `" . TABLE_PANEL_DOMAINS . "`
-					WHERE `phpsettingid` = :id
-					AND `parentdomainid` = '0'";
-			
-			if ((int) $userinfo['domains_see_all'] == 0) {
-				$query .= " AND `adminid` = :adminid";
-				$query_params['adminid'] = $userinfo['adminid'];
-			}
-			
-			if ((int) Settings::Get('panel.phpconfigs_hidestdsubdomain') == 1) {
-				$ssdids_res = Database::query("
-					SELECT DISTINCT `standardsubdomain` FROM `" . TABLE_PANEL_CUSTOMERS . "`
-					WHERE `standardsubdomain` > 0 ORDER BY `standardsubdomain` ASC;");
-				$ssdids = array();
-				while ($ssd = $ssdids_res->fetch(PDO::FETCH_ASSOC)) {
-					$ssdids[] = $ssd['standardsubdomain'];
+		if (isset($result['count']) && $result['count'] > 0) {
+			foreach ($result['list'] as $row) {
+				if (isset($row['is_default']) && $row['is_default'] == true) {
+					$row['description'] = "<b>" . $row['description'] . "</b>";
 				}
-				if (count($ssdids) > 0) {
-					$query .= " AND `id` NOT IN (" . implode(', ', $ssdids) . ")";
+				$domains = "";
+				foreach ($row['domains'] as $configdomain) {
+					$domains .= $configdomain . "<br>";
 				}
+				$count++;
+				eval("\$tablecontent.=\"" . getTemplate("phpconfig/overview_overview") . "\";");
 			}
-			
-			$domainresult_stmt = Database::prepare($query);
-			Database::pexecute($domainresult_stmt, $query_params);
-			
-			$domains = '';
-			if (Database::num_rows() > 0) {
-				while ($row2 = $domainresult_stmt->fetch(PDO::FETCH_ASSOC)) {
-					$domains .= $row2['domain'] . '<br/>';
-				}
-			}
-			
-			// check whether we use that config as froxor-vhost config
-			if (Settings::Get('system.mod_fcgid_defaultini_ownvhost') == $row['id'] || Settings::Get('phpfpm.vhost_defaultini') == $row['id']) {
-				$domains .= Settings::Get('system.hostname');
-			}
-			
-			if ($domains == '') {
-				$domains = $lng['admin']['phpsettings']['notused'];
-			}
-			
-			// check whether this is our default config
-			if ((Settings::Get('system.mod_fcgid') == '1' && Settings::Get('system.mod_fcgid_defaultini') == $row['id']) || (Settings::Get('phpfpm.enabled') == '1' && Settings::Get('phpfpm.defaultini') == $row['id'])) {
-				$row['description'] = '<b>' . $row['description'] . '</b>';
-			}
-			
-			$count ++;
-			eval("\$tablecontent.=\"" . getTemplate("phpconfig/overview_overview") . "\";");
 		}
 		
-		$log->logAction(ADM_ACTION, LOG_INFO, "php.ini setting overview has been viewed by '" . $userinfo['loginname'] . "'");
 		eval("echo \"" . getTemplate("phpconfig/overview") . "\";");
 	}
 	
@@ -104,77 +60,11 @@ if ($page == 'overview') {
 		if ((int) $userinfo['change_serversettings'] == 1) {
 			
 			if (isset($_POST['send']) && $_POST['send'] == 'send') {
-				$description = validate($_POST['description'], 'description');
-				$phpsettings = validate(str_replace("\r\n", "\n", $_POST['phpsettings']), 'phpsettings', '/^[^\0]*$/');
-				
-				if (Settings::Get('system.mod_fcgid') == 1) {
-					$binary = makeCorrectFile(validate($_POST['binary'], 'binary'));
-					$file_extensions = validate($_POST['file_extensions'], 'file_extensions', '/^[a-zA-Z0-9\s]*$/');
-					$mod_fcgid_starter = validate($_POST['mod_fcgid_starter'], 'mod_fcgid_starter', '/^[0-9]*$/', '', array(
-						'-1',
-						''
-					));
-					$mod_fcgid_maxrequests = validate($_POST['mod_fcgid_maxrequests'], 'mod_fcgid_maxrequests', '/^[0-9]*$/', '', array(
-						'-1',
-						''
-					));
-					$mod_fcgid_umask = validate($_POST['mod_fcgid_umask'], 'mod_fcgid_umask', '/^[0-9]*$/');
-					// disable fpm stuff
-					$fpm_config_id = 1;
-					$fpm_enableslowlog = 0;
-					$fpm_reqtermtimeout = 0;
-					$fpm_reqslowtimeout = 0;
-					$fpm_pass_authorizationheader = 0;
-				} elseif (Settings::Get('phpfpm.enabled') == 1) {
-					$fpm_config_id = intval($_POST['fpmconfig']);
-					$fpm_enableslowlog = isset($_POST['phpfpm_enable_slowlog']) ? (int) $_POST['phpfpm_enable_slowlog'] : 0;
-					$fpm_reqtermtimeout = validate($_POST['phpfpm_reqtermtimeout'], 'phpfpm_reqtermtimeout', '/^([0-9]+)(|s|m|h|d)$/');
-					$fpm_reqslowtimeout = validate($_POST['phpfpm_reqslowtimeout'], 'phpfpm_reqslowtimeout', '/^([0-9]+)(|s|m|h|d)$/');
-					$fpm_pass_authorizationheader = isset($_POST['phpfpm_pass_authorizationheader']) ? (int) $_POST['phpfpm_pass_authorizationheader'] : 0;
-					// disable fcgid stuff
-					$binary = '/usr/bin/php-cgi';
-					$file_extensions = 'php';
-					$mod_fcgid_starter = 0;
-					$mod_fcgid_maxrequests = 0;
-					$mod_fcgid_umask = "022";
+				try {
+					PhpSettings::getLocal($userinfo, $_POST)->add();
+				} catch (Exception $e) {
+					dynamic_error($e->getMessage());
 				}
-				
-				if (strlen($description) == 0 || strlen($description) > 50) {
-					standard_error('descriptioninvalid');
-				}
-				
-				$ins_stmt = Database::prepare("
-					INSERT INTO `" . TABLE_PANEL_PHPCONFIGS . "` SET
-						`description` = :desc,
-						`binary` = :binary,
-						`file_extensions` = :fext,
-						`mod_fcgid_starter` = :starter,
-						`mod_fcgid_maxrequests` = :mreq,
-						`mod_fcgid_umask` = :umask,
-						`fpm_slowlog` = :fpmslow,
-						`fpm_reqterm` = :fpmreqterm,
-						`fpm_reqslow` = :fpmreqslow,
-						`phpsettings` = :phpsettings,
-						`fpmsettingid` = :fpmsettingid,
-						`pass_authorizationheader` = :fpmpassauth");
-				$ins_data = array(
-					'desc' => $description,
-					'binary' => $binary,
-					'fext' => $file_extensions,
-					'starter' => $mod_fcgid_starter,
-					'mreq' => $mod_fcgid_maxrequests,
-					'umask' => $mod_fcgid_umask,
-					'fpmslow' => $fpm_enableslowlog,
-					'fpmreqterm' => $fpm_reqtermtimeout,
-					'fpmreqslow' => $fpm_reqslowtimeout,
-					'phpsettings' => $phpsettings,
-					'fpmsettingid' => $fpm_config_id,
-					'fpmpassauth' => $fpm_pass_authorizationheader
-				);
-				Database::pexecute($ins_stmt, $ins_data);
-				
-				inserttask('1');
-				$log->logAction(ADM_ACTION, LOG_INFO, "php.ini setting with description '" . $description . "' has been created by '" . $userinfo['loginname'] . "'");
 				redirectTo($filename, array(
 					'page' => $page,
 					's' => $s
@@ -204,42 +94,23 @@ if ($page == 'overview') {
 	}
 	
 	if ($action == 'delete') {
-		
-		$result_stmt = Database::prepare("
-			SELECT * FROM `" . TABLE_PANEL_PHPCONFIGS . "` WHERE `id` = :id");
-		$result = Database::pexecute_first($result_stmt, array(
-			'id' => $id
-		));
-		
-		if ((Settings::Get('system.mod_fcgid') == '1' && Settings::Get('system.mod_fcgid_defaultini_ownvhost') == $id) || (Settings::Get('phpfpm.enabled') == '1' && Settings::Get('phpfpm.vhost_defaultini') == $id)) {
-			standard_error('cannotdeletehostnamephpconfig');
+
+		try {
+			$json_result = PhpSettings::getLocal($userinfo, array('id' => $id))->get();
+		} catch (Exception $e) {
+			dynamic_error($e->getMessage());
 		}
-		
-		if ((Settings::Get('system.mod_fcgid') == '1' && Settings::Get('system.mod_fcgid_defaultini') == $id) || (Settings::Get('phpfpm.enabled') == '1' && Settings::Get('phpfpm.defaultini') == $id)) {
-			standard_error('cannotdeletedefaultphpconfig');
-		}
+		$result = json_decode($json_result, true)['data'];
 		
 		if ($result['id'] != 0 && $result['id'] == $id && (int) $userinfo['change_serversettings'] == 1 && $id != 1) // cannot delete the default php.config
 		{
 			
 			if (isset($_POST['send']) && $_POST['send'] == 'send') {
-				// set php-config to default for all domains using the
-				// config that is to be deleted
-				$upd_stmt = Database::prepare("
-					UPDATE `" . TABLE_PANEL_DOMAINS . "` SET
-					`phpsettingid` = '1' WHERE `phpsettingid` = :id");
-				Database::pexecute($upd_stmt, array(
-					'id' => $id
-				));
-				
-				$del_stmt = Database::prepare("
-					DELETE FROM `" . TABLE_PANEL_PHPCONFIGS . "` WHERE `id` = :id");
-				Database::pexecute($del_stmt, array(
-					'id' => $id
-				));
-				
-				inserttask('1');
-				$log->logAction(ADM_ACTION, LOG_INFO, "php.ini setting with id #" . (int) $id . " has been deleted by '" . $userinfo['loginname'] . "'");
+				try {
+					PhpSettings::getLocal($userinfo, array('id' => $id))->delete();
+				} catch (Exception $e) {
+					dynamic_error($e->getMessage());
+				}
 				redirectTo($filename, array(
 					'page' => $page,
 					's' => $s
@@ -258,103 +129,36 @@ if ($page == 'overview') {
 	
 	if ($action == 'edit') {
 		
-		$result_stmt = Database::prepare("
-			SELECT * FROM `" . TABLE_PANEL_PHPCONFIGS . "` WHERE `id` = :id");
-		$result = Database::pexecute_first($result_stmt, array(
-			'id' => $id
-		));
+		try {
+			$json_result = PhpSettings::getLocal($userinfo, array('id' => $id))->get();
+		} catch (Exception $e) {
+			dynamic_error($e->getMessage());
+		}
+		$result = json_decode($json_result, true)['data'];
 		
 		if ($result['id'] != 0 && $result['id'] == $id && (int) $userinfo['change_serversettings'] == 1) {
 			
 			if (isset($_POST['send']) && $_POST['send'] == 'send') {
-				$description = validate($_POST['description'], 'description');
-				$phpsettings = validate(str_replace("\r\n", "\n", $_POST['phpsettings']), 'phpsettings', '/^[^\0]*$/');
-				
-				if (Settings::Get('system.mod_fcgid') == 1) {
-					$binary = makeCorrectFile(validate($_POST['binary'], 'binary'));
-					$file_extensions = validate($_POST['file_extensions'], 'file_extensions', '/^[a-zA-Z0-9\s]*$/');
-					$mod_fcgid_starter = validate($_POST['mod_fcgid_starter'], 'mod_fcgid_starter', '/^[0-9]*$/', '', array(
-						'-1',
-						''
-					));
-					$mod_fcgid_maxrequests = validate($_POST['mod_fcgid_maxrequests'], 'mod_fcgid_maxrequests', '/^[0-9]*$/', '', array(
-						'-1',
-						''
-					));
-					$mod_fcgid_umask = validate($_POST['mod_fcgid_umask'], 'mod_fcgid_umask', '/^[0-9]*$/');
-					// disable fpm stuff
-					$fpm_config_id = 1;
-					$fpm_enableslowlog = 0;
-					$fpm_reqtermtimeout = 0;
-					$fpm_reqslowtimeout = 0;
-					$fpm_pass_authorizationheader = 0;
-				} elseif (Settings::Get('phpfpm.enabled') == 1) {
-					$fpm_config_id = intval($_POST['fpmconfig']);
-					$fpm_enableslowlog = isset($_POST['phpfpm_enable_slowlog']) ? (int) $_POST['phpfpm_enable_slowlog'] : 0;
-					$fpm_reqtermtimeout = validate($_POST['phpfpm_reqtermtimeout'], 'phpfpm_reqtermtimeout', '/^([0-9]+)(|s|m|h|d)$/');
-					$fpm_reqslowtimeout = validate($_POST['phpfpm_reqslowtimeout'], 'phpfpm_reqslowtimeout', '/^([0-9]+)(|s|m|h|d)$/');
-					$fpm_pass_authorizationheader = isset($_POST['phpfpm_pass_authorizationheader']) ? (int) $_POST['phpfpm_pass_authorizationheader'] : 0;
-					// disable fcgid stuff
-					$binary = '/usr/bin/php-cgi';
-					$file_extensions = 'php';
-					$mod_fcgid_starter = 0;
-					$mod_fcgid_maxrequests = 0;
-					$mod_fcgid_umask = "022";
+				try {
+					PhpSettings::getLocal($userinfo, $_POST)->update();
+				} catch (Exception $e) {
+					dynamic_error($e->getMessage());
 				}
-				
-				if (strlen($description) == 0 || strlen($description) > 50) {
-					standard_error('descriptioninvalid');
-				}
-				
-				$upd_stmt = Database::prepare("
-					UPDATE `" . TABLE_PANEL_PHPCONFIGS . "` SET
-						`description` = :desc,
-						`binary` = :binary,
-						`file_extensions` = :fext,
-						`mod_fcgid_starter` = :starter,
-						`mod_fcgid_maxrequests` = :mreq,
-						`mod_fcgid_umask` = :umask,
-						`fpm_slowlog` = :fpmslow,
-						`fpm_reqterm` = :fpmreqterm,
-						`fpm_reqslow` = :fpmreqslow,
-						`phpsettings` = :phpsettings,
-						`fpmsettingid` = :fpmsettingid,
-						`pass_authorizationheader` = :fpmpassauth
-					WHERE `id` = :id");
-				$upd_data = array(
-					'desc' => $description,
-					'binary' => $binary,
-					'fext' => $file_extensions,
-					'starter' => $mod_fcgid_starter,
-					'mreq' => $mod_fcgid_maxrequests,
-					'umask' => $mod_fcgid_umask,
-					'fpmslow' => $fpm_enableslowlog,
-					'fpmreqterm' => $fpm_reqtermtimeout,
-					'fpmreqslow' => $fpm_reqslowtimeout,
-					'phpsettings' => $phpsettings,
-					'fpmsettingid' => $fpm_config_id,
-					'fpmpassauth' => $fpm_pass_authorizationheader,
-					'id' => $id
-				);
-				Database::pexecute($upd_stmt, $upd_data);
-				
-				inserttask('1');
-				$log->logAction(ADM_ACTION, LOG_INFO, "php.ini setting with description '" . $description . "' has been changed by '" . $userinfo['loginname'] . "'");
 				redirectTo($filename, array(
 					'page' => $page,
 					's' => $s
 				));
 			} else {
-
+				
 				$fpmconfigs = '';
 				$configs = Database::query("SELECT * FROM `" . TABLE_PANEL_FPMDAEMONS . "` ORDER BY `description` ASC");
 				while ($row = $configs->fetch(PDO::FETCH_ASSOC)) {
 					$fpmconfigs .= makeoption($row['description'], $row['id'], $result['fpmsettingid'], true, true);
 				}
-
+				
 				$phpconfig_edit_data = include_once dirname(__FILE__) . '/lib/formfields/admin/phpconfig/formfield.phpconfig_edit.php';
 				$phpconfig_edit_form = htmlform::genHTMLForm($phpconfig_edit_data);
-
+				
 				$title = $phpconfig_edit_data['phpconfig_edit']['title'];
 				$image = $phpconfig_edit_data['phpconfig_edit']['image'];
 				
@@ -373,7 +177,7 @@ if ($page == 'overview') {
 		$result = Database::query("SELECT * FROM `" . TABLE_PANEL_FPMDAEMONS . "` ORDER BY `description` ASC");
 		
 		while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-
+			
 			$query_params = array(
 				'id' => $row['id']
 			);
@@ -461,9 +265,9 @@ if ($page == 'overview') {
 			} else {
 				
 				$pm_select = makeoption('static', 'static', 'static', true, true);
-				$pm_select.= makeoption('dynamic', 'dynamic', 'static', true, true);
-				$pm_select.= makeoption('ondemand', 'ondemand', 'static', true, true);
-
+				$pm_select .= makeoption('dynamic', 'dynamic', 'static', true, true);
+				$pm_select .= makeoption('ondemand', 'ondemand', 'static', true, true);
+				
 				$fpmconfig_add_data = include_once dirname(__FILE__) . '/lib/formfields/admin/phpconfig/formfield.fpmconfig_add.php';
 				$fpmconfig_add_form = htmlform::genHTMLForm($fpmconfig_add_data);
 				
@@ -484,11 +288,11 @@ if ($page == 'overview') {
 		$result = Database::pexecute_first($result_stmt, array(
 			'id' => $id
 		));
-
+		
 		if ($id == 1) {
 			standard_error('cannotdeletedefaultphpconfig');
 		}
-
+		
 		if ($result['id'] != 0 && $result['id'] == $id && (int) $userinfo['change_serversettings'] == 1 && $id != 1) // cannot delete the default php.config
 		{
 			
@@ -592,9 +396,9 @@ if ($page == 'overview') {
 			} else {
 				
 				$pm_select = makeoption('static', 'static', $result['pm'], true, true);
-				$pm_select.= makeoption('dynamic', 'dynamic', $result['pm'], true, true);
-				$pm_select.= makeoption('ondemand', 'ondemand', $result['pm'], true, true);
-
+				$pm_select .= makeoption('dynamic', 'dynamic', $result['pm'], true, true);
+				$pm_select .= makeoption('ondemand', 'ondemand', $result['pm'], true, true);
+				
 				$fpmconfig_edit_data = include_once dirname(__FILE__) . '/lib/formfields/admin/phpconfig/formfield.fpmconfig_edit.php';
 				$fpmconfig_edit_form = htmlform::genHTMLForm($fpmconfig_edit_data);
 				
