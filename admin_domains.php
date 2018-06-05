2ba4137e7da5917db676b674d412fe2f39ce0269
Daniel Reichelt <hacking@nachtgeist.net>
2018-06-05 06:51:20 +0200

    fix triggering an LE CSR when changing www on a main domain
    Prior to this, LE CSRs were triggered only when the wwwserveralias was
    changed on alias domains, but not on main domains.
    
    Fixes #526

--- admin_domains.php
+++ admin_domains.php
@@ -2027,7 +2027,15 @@ if ($page == 'domains' || $page == 'overview') {
 				} else
 					if ($result['wwwserveralias'] != $wwwserveralias || $result['letsencrypt'] != $letsencrypt) {
 						// or when wwwserveralias or letsencrypt was changed
+
 						triggerLetsEncryptCSRForAliasDestinationDomain($aliasdomain, $log);
+
+						if ($aliasdomain === 0) {
+							// in case the wwwserveralias is set on a main domain, $aliasdomain is 0
+							// --> the call just above to triggerLetsEncryptCSRForAliasDestinationDomain
+							//     is a noop...let's repeat it with the domain id of the main domain
+							triggerLetsEncryptCSRForAliasDestinationDomain($id, $log);
+						}
 					}
 
 				$log->logAction(ADM_ACTION, LOG_INFO, "edited domain #" . $id);
