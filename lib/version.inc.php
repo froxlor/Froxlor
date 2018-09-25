095e54d88dfdad2a1e7c73c4ed2b9435c00fe4ee
Michael Kaufmann <d00p@froxlor.org>
2018-09-25 08:17:31 +0200

    fix missing update in version.inc.php
    Signed-off-by: Michael Kaufmann <d00p@froxlor.org>

--- lib/version.inc.php
+++ lib/version.inc.php
@@ -19,7 +19,7 @@
 $version = '0.9.39.5';
 
 // Database version (YYYYMMDDC where C is a daily counter)
-$dbversion = '201805290';
+$dbversion = '201809180';
 
 // Distribution branding-tag (used for Debian etc.)
 $branding = '';
