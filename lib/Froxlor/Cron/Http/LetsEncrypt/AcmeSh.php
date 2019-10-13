5d5cc3dda32515fc7bbe434c3f7c75e81a45228c
Michael Kaufmann <d00p@froxlor.org>
2019-10-13 22:49:03 +0200

    fix array to string conversion
    Signed-off-by: Michael Kaufmann <d00p@froxlor.org>

--- lib/Froxlor/Cron/Http/LetsEncrypt/AcmeSh.php
+++ lib/Froxlor/Cron/Http/LetsEncrypt/AcmeSh.php
@@ -389,7 +389,8 @@ class AcmeSh extends \Froxlor\Cron\FroxlorCron
 	{
 		$acmesh_result = \Froxlor\FileDir::safe_exec(self::$acmesh . " --upgrade");
 		// check for activated cron (which is installed automatically) but we don't need it
-		$acmesh_result .= \Froxlor\FileDir::safe_exec(self::$acmesh . " --uninstall-cronjob");
+		$acmesh_result2 = \Froxlor\FileDir::safe_exec(self::$acmesh . " --uninstall-cronjob");
+		array_push($acmesh_result, $acmesh_result2);
 		FroxlorLogger::getInstanceOf()->logAction(FroxlorLogger::CRON_ACTION, LOG_INFO, "Checking for LetsEncrypt client upgrades before renewing certificates:\n" . implode("\n", $acmesh_result));
 	}
 }
