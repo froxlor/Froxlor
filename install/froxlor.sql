6cd061d74c3b40b49f0fca14853bbebf18f2f8bb
Michael Kaufmann <d00p@froxlor.org>
2019-11-01 11:16:13 +0100

    set version to 0.10.4 for upcoming maintenance release; minor code formatting + adjustments
    Signed-off-by: Michael Kaufmann <d00p@froxlor.org>

--- install/froxlor.sql
+++ install/froxlor.sql
@@ -695,7 +695,7 @@ opcache.interned_strings_buffer'),
 	('panel', 'password_special_char', '!?<>§$%+#=@'),
 	('panel', 'customer_hide_options', ''),
 	('panel', 'is_configured', '0'),
-	('panel', 'version', '0.10.3'),
+	('panel', 'version', '0.10.4'),
 	('panel', 'db_version', '201910200');
 
 
