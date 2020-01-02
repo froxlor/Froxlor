e01c2e02fba86f2aa6907ad972063ecd40bf2f95
Michael Kaufmann <d00p@froxlor.org>
2020-01-02 12:28:48 +0100

    fix update query for new fpm-custom_config value, refs #783
    Signed-off-by: Michael Kaufmann <d00p@froxlor.org>

--- install/updates/froxlor/0.10/update_0.10.inc.php
+++ install/updates/froxlor/0.10/update_0.10.inc.php
@@ -533,7 +533,7 @@ if (\Froxlor\Froxlor::isDatabaseVersion('201912100')) {
 
 if (\Froxlor\Froxlor::isDatabaseVersion('201912310')) {
 	showUpdateStep("Adding custom phpfpm pool configuration field");
-	Database::query("ALTER TABLE `" . TABLE_PANEL_PHPDAEMONS . "` ADD `custom_config` text AFTER `limit_extensions`;");
+	Database::query("ALTER TABLE `" . TABLE_PANEL_FPMDAEMONS . "` ADD `custom_config` text AFTER `limit_extensions`;");
 	lastStepStatus(0);
 	\Froxlor\Froxlor::updateToDbVersion('201912311');
 }
