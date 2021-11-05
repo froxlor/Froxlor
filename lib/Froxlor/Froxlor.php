46df429909dec540dfb84e2dc2b9ea21702318d7
Michael Kaufmann <d00p@froxlor.org>
2021-11-05 09:27:58 +0100

    set version to 0.10.30 for upcoming release
    Signed-off-by: Michael Kaufmann <d00p@froxlor.org>

--- lib/Froxlor/Froxlor.php
+++ lib/Froxlor/Froxlor.php
@@ -7,7 +7,7 @@ final class Froxlor
 {
 
 	// Main version variable
-	const VERSION = '0.10.29.1';
+	const VERSION = '0.10.30';
 
 	// Database version (YYYYMMDDC where C is a daily counter)
 	const DBVERSION = '202109040';
