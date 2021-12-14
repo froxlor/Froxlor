5ccae3f9bb8545ca6c08168779f8639e76598bce
Michael Kaufmann <d00p@froxlor.org>
2021-12-14 11:23:36 +0100

    do not check for allowed-phpconfigs if fpm/fcgid is not activated (no possibility to select a config anyway)
    Signed-off-by: Michael Kaufmann <d00p@froxlor.org>

--- lib/Froxlor/Api/Commands/SubDomains.php
+++ lib/Froxlor/Api/Commands/SubDomains.php
@@ -268,8 +268,11 @@ class SubDomains extends \Froxlor\Api\ApiCommand implements \Froxlor\Api\Resourc
 			} else {
 				$allowed_phpconfigs = [];
 			}
-			if (! in_array($phpsid_result['phpsettingid'], $allowed_phpconfigs)) {
-				\Froxlor\UI\Response::standard_error('notallowedphpconfigused', '', true);
+			// only with fcgid/fpm enabled will it be possible to select a php-setting
+			if ((int) Settings::Get('system.mod_fcgid') == 1 || (int) Settings::Get('phpfpm.enabled') == 1) {
+				if (! in_array($phpsid_result['phpsettingid'], $allowed_phpconfigs)) {
+					\Froxlor\UI\Response::standard_error('notallowedphpconfigused', '', true);
+				}
 			}
 
 			// actually insert domain
@@ -654,8 +657,11 @@ class SubDomains extends \Froxlor\Api\ApiCommand implements \Froxlor\Api\Resourc
 		} else {
 			$allowed_phpconfigs = [];
 		}
-		if (! in_array($phpsettingid, $allowed_phpconfigs)) {
-			\Froxlor\UI\Response::standard_error('notallowedphpconfigused', '', true);
+		// only with fcgid/fpm enabled will it be possible to select a php-setting
+		if ((int) Settings::Get('system.mod_fcgid') == 1 || (int) Settings::Get('phpfpm.enabled') == 1) {
+			if (! in_array($phpsettingid, $allowed_phpconfigs)) {
+				\Froxlor\UI\Response::standard_error('notallowedphpconfigused', '', true);
+			}
 		}
 
 		// handle redirect
