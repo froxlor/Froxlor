b4d9b1037d749e204c143c0d83ce23b7d18b8116
envoyr <hello@envoyr.com>
2022-02-17 15:30:05 +0100

    add dark mode

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,4 +4,5 @@ let mix = require('laravel-mix');
 
 mix
     .js('templates/Froxlor/src/main.js', 'templates/Froxlor/assets/js')
-    .sass('templates/Froxlor/src/scss/main.scss', 'templates/Froxlor/assets/css');
+    .sass('templates/Froxlor/src/scss/main.scss', 'templates/Froxlor/assets/css')
+    .sass('templates/Froxlor/src/scss/dark.scss', 'templates/Froxlor/assets/css');
