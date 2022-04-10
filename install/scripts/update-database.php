a475deee24280e8f1fc32e83ea5d7c2230fe008b
Dennis Dudek <admin@kapsonfire.de>
2022-04-10 11:17:38 +0200

    Create update-database.php
    allows to execute the upgrade process via clia

---
+++ install/scripts/update-database.php
@@ -0,0 +1,23 @@
+<?php
+// Include autoloader and table constants
+require __DIR__.'/../../vendor/autoload.php';
+require \Froxlor\Froxlor::getInstallDir() . '/lib/tables.inc.php';
+
+// remove html for console output
+function cleanUpdateOutput($buffer) {
+        static $newLine = true;
+        $buffer = strip_tags(preg_replace("/<br\W*?\/>/", "\n", $buffer));
+        if($newLine) {
+                $buffer = date('[H:i:s] ').$buffer;
+        }
+
+        $newLine = strpos($buffer, "\n") !== false              ;
+        return $buffer;
+}
+
+define('_CRON_UPDATE', 1);
+ob_start('cleanUpdateOutput', 2); //force direct flushing
+
+// execute update scripts
+include_once \Froxlor\Froxlor::getInstallDir() . '/install/updatesql.php';
+ob_end_flush();
