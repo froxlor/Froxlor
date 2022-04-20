8c942178d26c8da3c232d5420247edf3032ee304
envoyr <hello@envoyr.com>
2022-04-20 17:01:25 +0200

    fix font assets path, logo path, database installation and checks

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,6 +3,12 @@
 let mix = require('laravel-mix');
 
 mix
-    .js('templates/Froxlor/src/js/main.js', 'templates/Froxlor/assets/js')
-    .sass('templates/Froxlor/src/scss/main.scss', 'templates/Froxlor/assets/css')
-    .sass('templates/Froxlor/src/scss/dark.scss', 'templates/Froxlor/assets/css');
+	.setPublicPath('')
+	.options({
+		fileLoaderDirs:  {
+			fonts: 'templates/Froxlor/assets/fonts'
+		}
+	})
+	.js('templates/Froxlor/src/js/main.js', 'templates/Froxlor/assets/js')
+	.sass('templates/Froxlor/src/scss/main.scss', 'templates/Froxlor/assets/css')
+	.sass('templates/Froxlor/src/scss/dark.scss', 'templates/Froxlor/assets/css');
