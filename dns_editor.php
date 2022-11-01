ec0094e75415e42099b7c18293c6a2c78455d4db
Michael Kaufmann <d00p@froxlor.org>
2022-11-01 13:38:27 +0100

    UI/UX fixes in dns-editor and logfiles-viewer
    Signed-off-by: Michael Kaufmann <d00p@froxlor.org>

--- dns_editor.php
+++ dns_editor.php
@@ -125,7 +125,6 @@ UI::view('user/dns-editor.html.twig', [
 	'listing' => Listing::format($collection, $dns_list_data, 'dns_list', ['domain_id' => $domain_id]),
 	'actions_links' => [
 		[
-			'class' => 'btn-outline-secondary',
 			'href' => $linker->getLink([
 				'section' => 'domains',
 				'page' => 'domains',
@@ -136,10 +135,9 @@ UI::view('user/dns-editor.html.twig', [
 			'icon' => 'fa-solid fa-pen'
 		],
 		[
-			'class' => 'btn-outline-primary',
 			'href' => $linker->getLink(['section' => 'domains', 'page' => 'domains']),
-			'label' => lng('menue.domains.domains'),
-			'icon' => 'fa-solid fa-globe'
+			'label' => lng('panel.backtooverview'),
+			'icon' => 'fa-solid fa-reply'
 		]
 	],
 	'formaction' => $linker->getLink(['section' => 'domains', 'action' => 'add_record', 'domain_id' => $domain_id]),
