c5bece64ce29920321d36de2a3ed3cab4595fc1c
Michael Kaufmann <d00p@froxlor.org>
2023-01-28 20:07:15 +0100

    set version to 2.0.10 for security release
    Signed-off-by: Michael Kaufmann <d00p@froxlor.org>

--- lib/Froxlor/Froxlor.php
+++ lib/Froxlor/Froxlor.php
@@ -31,7 +31,7 @@ final class Froxlor
 {
 
 	// Main version variable
-	const VERSION = '2.0.9';
+	const VERSION = '2.0.10';
 
 	// Database version (YYYYMMDDC where C is a daily counter)
 	const DBVERSION = '202301180';
