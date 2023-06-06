9ed45ea7f8c7fef33a832211dfd0e2d0d04413b4
Michael Kaufmann <d00p@froxlor.org>
2023-06-06 09:46:31 +0200

    make alpha to dev, version check does not know about 'alpha'
    Signed-off-by: Michael Kaufmann <d00p@froxlor.org>

--- lib/Froxlor/Froxlor.php
+++ lib/Froxlor/Froxlor.php
@@ -31,7 +31,7 @@ final class Froxlor
 {
 
 	// Main version variable
-	const VERSION = '2.1.0-alpha1';
+	const VERSION = '2.1.0-dev1';
 
 	// Database version (YYYYMMDDC where C is a daily counter)
 	const DBVERSION = '202305240';
