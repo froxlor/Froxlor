6d9014c29b14673dfe69d4ad705f1d98ac0092ba
Michael Kaufmann <d00p@froxlor.org>
2023-09-07 15:34:06 +0200

    fix API permission error in navigation when customer-hide-options include 'domains'; fixes #1183
    Signed-off-by: Michael Kaufmann <d00p@froxlor.org>

--- lib/Froxlor/CurrentUser.php
+++ lib/Froxlor/CurrentUser.php
@@ -151,9 +151,13 @@ class CurrentUser
 			]);
 			$addition = $result['emaildomains'] != 0;
 		} elseif ($resource == 'subdomains') {
-			$parentDomainCollection = (new Collection(SubDomains::class, $_SESSION['userinfo'],
-				['sql_search' => ['d.parentdomainid' => 0]]));
-			$addition = $parentDomainCollection->count() != 0;
+			if (Settings::IsInList('panel.customer_hide_options', 'domains')) {
+				$addition = false;
+			} else {
+				$parentDomainCollection = (new Collection(SubDomains::class, $_SESSION['userinfo'],
+					['sql_search' => ['d.parentdomainid' => 0]]));
+				$addition = $parentDomainCollection->count() != 0;
+			}
 		} elseif ($resource == 'domains') {
 			$customerCollection = (new Collection(Customers::class, $_SESSION['userinfo']));
 			$addition = $customerCollection != 0;
