c98e912fc55b7a250c440df2a18a6701976f0280
Michael Kaufmann <d00p@froxlor.org>
2023-11-10 14:35:44 +0100

    add description for 'disable_otp_security_check' flag in config.example.inc.php
    Signed-off-by: Michael Kaufmann <d00p@froxlor.org>

--- lib/config.example.inc.php
+++ lib/config.example.inc.php
@@ -9,11 +9,18 @@ return [
 	 * recommended value for debian/ubuntu package users is false to rely on apt and not have version mixup.
 	 * This is also useful for providers that manage the servers but give admin access to froxlor to handle
 	 * updates the way the providers does it (e.g. automation, etc.)
+	 *
+	 * Default: false
 	 */
 	'enable_webupdate' => false,
 
 	/**
-	 * @todo description
+	 * settings that have a major impact on the system or which values are used to be executed with high
+	 * privileges on the system require the admin-user to have set up and enabled OTP for the corresponding
+	 * account to change these values.
+	 * To disable this extra security validation, set the value of this to true
+	 *
+	 * Default: false
 	 */
 	'disable_otp_security_check' => false,
 ];
