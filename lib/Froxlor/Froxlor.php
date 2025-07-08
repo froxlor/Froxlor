97360e450d8ecb528e0764a8416b56814e89652c
Michael Kaufmann <d00p@froxlor.org>
2025-07-08 09:02:19 +0200

    set version to 2.2.8 for maintenance release
    Signed-off-by: Michael Kaufmann <d00p@froxlor.org>

--- lib/Froxlor/Froxlor.php
+++ lib/Froxlor/Froxlor.php
@@ -31,7 +31,7 @@ final class Froxlor
 {
 
 	// Main version variable
-	const VERSION = '2.2.7';
+	const VERSION = '2.2.8';
 
 	// Database version (YYYYMMDDC where C is a daily counter)
 	const DBVERSION = '202412030';
