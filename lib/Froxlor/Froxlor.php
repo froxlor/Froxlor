c2a7a2ac36e6d4934cb58eeb50fcffc44f61a3cb
Michael Kaufmann <d00p@froxlor.org>
2025-12-28 23:50:50 +0100

    set version to 2.3.2 for upcoming bugfix release
    Signed-off-by: Michael Kaufmann <d00p@froxlor.org>

--- lib/Froxlor/Froxlor.php
+++ lib/Froxlor/Froxlor.php
@@ -31,7 +31,7 @@ final class Froxlor
 {
 
 	// Main version variable
-	const VERSION = '2.3.1';
+	const VERSION = '2.3.2';
 
 	// Database version (YYYYMMDDC where C is a daily counter)
 	const DBVERSION = '202512280';
