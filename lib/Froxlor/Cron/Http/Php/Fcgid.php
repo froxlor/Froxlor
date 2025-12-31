aa8fa4a8600880e623a2fae6194a29e8c133e1e4
Michael Kaufmann <d00p@froxlor.org>
2025-12-31 09:11:21 +0100

    community tested - fcgid domain folder also needs (back) 0755 permissions, thx Stav
    Signed-off-by: Michael Kaufmann <d00p@froxlor.org>

--- lib/Froxlor/Cron/Http/Php/Fcgid.php
+++ lib/Froxlor/Cron/Http/Php/Fcgid.php
@@ -130,7 +130,7 @@ class Fcgid
 		if (!is_dir($configdir) && $createifnotexists) {
 			FileDir::safe_exec('mkdir -p ' . escapeshellarg($configdir));
 			FileDir::safe_exec('chmod 0755 ' . escapeshellarg(dirname($configdir)));
-			FileDir::safe_exec('chmod 0750 ' . escapeshellarg($configdir));
+			FileDir::safe_exec('chmod 0755 ' . escapeshellarg($configdir));
 			FileDir::safe_exec('chown ' . $this->domain['guid'] . ':' . $this->domain['guid'] . ' ' . escapeshellarg($configdir));
 		}
 
